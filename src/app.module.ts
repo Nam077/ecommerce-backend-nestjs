5f9959c5b32da6b575d59c1667f0442305928a54
Nam077 <nam077.me@gmail.com>
2023-05-29 02:24:55 +0700

    generate module role

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './modules/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
+import { RoleModule } from './modules/role/role.module';
 
 @Module({
     imports: [
@@ -13,6 +14,7 @@ import { UserModule } from './modules/user/user.module';
         }),
         DatabaseModule,
         UserModule,
+        RoleModule,
     ],
     controllers: [AppController],
     providers: [AppService],
