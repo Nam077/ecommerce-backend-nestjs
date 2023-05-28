f205877f85910a26902286aa48646cf5743af284
Nam077 <nam077.me@gmail.com>
2023-05-29 03:02:46 +0700

    make common hash and slug

--- src/common/slug.ts
+++ src/common/slug.ts
@@ -1,1 +1,9 @@
-export class Slug {}
+import slugify from 'slugify';
+
+export class Slug {
+    public static slugify(value: string): string {
+        return slugify(value, {
+            replacement: '-',
+        });
+    }
+}
